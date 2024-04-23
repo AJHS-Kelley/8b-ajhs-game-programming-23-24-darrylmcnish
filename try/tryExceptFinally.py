de9a672f10e4c900fd1a8e92b0782365f4c7c237
darrylmcnish <s11959608@students.duvalschools.org>
2024-04-23 13:48:16 -0400

    Create tryExceptFinally.py

---
+++ try/tryExceptFinally.py
@@ -0,0 +1,17 @@
+#This is a method for testing code and preventing crashes
+#Try -- except -- else -- finally
+
+try: # The code in this block is ALWAYS executed
+    myVariable = 1
+    print(myVariabl1)
+except NameError:
+    print("There is an incorrect variable name in your code.")
+except: # This code will run IF there is an error (exeption)
+    print("something has gone wrong!")
+else: # This code runs if there are NO ERRORS
+    print("code executed correctly with no exceptions.\n")
+finally: #THIS CODE WILL ALWAYS RUN, ITS LIKE THE TERMINATOR
+    print("I'll be back")
+
+#except NameError:
+#    pass
