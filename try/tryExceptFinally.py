2116f95a0a21a9b9539a78573bc8fb47a5e0e9d3
darrylmcnish <s11959608@students.duvalschools.org>
2024-04-23 14:23:51 -0400

    Update tryExceptFinally.py

--- try/tryExceptFinally.py
+++ try/tryExceptFinally.py
@@ -3,7 +3,9 @@
 
 try: # The code in this block is ALWAYS executed
     myVariable = 1
-    print(myVariabl1)
+    print(myVariable)
+    myString = "Five"
+    print(float(myString))
 except NameError:
     print("There is an incorrect variable name in your code.")
 except: # This code will run IF there is an error (exeption)
