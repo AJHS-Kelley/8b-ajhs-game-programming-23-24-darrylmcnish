e4943794e3772082d8a6d4f663f96a40e985cb18
darrylmcnish <s11959608@students.duvalschools.org>
2024-04-25 14:45:11 -0400

    Update finalProject.py

--- 09_finalProject/finalProject.py
+++ 09_finalProject/finalProject.py
@@ -11,19 +11,19 @@ else:
     x = 1920
     y = 1080
 
-pygame.int()
+#pygame.int()
 
 difficulty = int(input("please select a difficulty. Enter 1 foe Easy or 2 for HARD. \n"))
 
 if difficulty == 1:
-    pygame.display.set_caption('NAME OF GAME -- EASY')
-else:
-    pygame.display.set_caption('NAME OF GAME -- HARD')
+#    pygame.display.set_caption('NAME OF GAME -- EASY')
+#else:
+#    pygame.display.set_caption('NAME OF GAME -- HARD')
     
-screen = pygame.display.set_caption('NAME OF GAME -- EASY')
-screen = pygame.display.set_caption('NAME OF GAME -- HARD')
+#screen = pygame.display.set_caption('NAME OF GAME -- EASY')
+#screen = pygame.display.set_caption('NAME OF GAME -- HARD')
 
-screen = pygame.display.set_mode((x, y))
+ screen = pygame.display.set_mode((x, y))
 #CREATE AN if / else BLOCK TO SET RESOLUTION BASED ON THE VARIABLE ABOVE.
 
 
@@ -46,12 +46,12 @@ RED = (255, 0, 0)
 #CAR
 car_width = 60
 cra_height = 80
-car_img = pygame.image.load('car.png')
+car_img = pygame.image.load#('car.png')
 
 #OPPONENT CAR
 opponent_width = 60
 opponent_height = 80
-opponent_img = pygame.image.load('opponent_car.png')
+opponent_img = pygame.image.load#('opponent_car.png')
 
 #clock
 clock = pygame.time.Clock()
